bc477330ca94059f70b24a3afcb44d742a858abc
pcovinos1 <pcovinos@morada.pe>
2026-09-04 21:47:51 -0500

    Fix Bustamante175 Pages asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Bustamante175/",
   plugins: [react()],
   build: {
     sourcemap: true
